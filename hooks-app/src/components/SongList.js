fef6857a061c036576705854fc902a605047a540
abdalhadi bakr <hade.bk.1998@gmail.com>
2020-05-27 21:45:16 +0200

    using useState & useEffect

--- hooks-app/src/components/SongList.js
+++ hooks-app/src/components/SongList.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { v1 as id } from 'uuid';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
+    /**
+     * songs => used ro reference the objects in state, like (songs[0].title => Sky Full)
+     * setSongs => used to set new values to state.
+     * useState => used to init the state, of course you can assign to it init data, like ex down 
+     */
+    const [songs, setSongs] = useState([
+        {title: 'Sky Full', id: 1},
+        {title: 'Magic', id: 2},
+        {title: 'Yellow', id: 3},
+    ]);
+
+    const [songsCount, setSongsCount] = useState(3);
+
+    /**
+     * useEffect => run when data changed, ex: when add new song..
+     * callback fun help to determine when we need to run useEffect (avoid always running of useEffect).
+     * in this ex useEffect will run only when songs will change.
+     */
+    useEffect(() => {
+        console.log('useEffect run', songs)
+    }, [songs]);
+
+    
+    // add new song to list
+    const addNewSong = (title) => {
+        setSongs([...songs, {title, id:id()}])
+    }
+
     return ( 
         <div className='song-list'>
             <ul>
-                <li>Song 1</li>
-                <li>Song 2</li>
-                <li>Song 3</li>
+                {songs.map((song) => {
+                    return(
+                        <li key={song.id}>{ song.title }</li>
+                    )
+                })}
             </ul>
+            <NewSongForm addNewSong={addNewSong}/>
+            <button style={{marginTop:'20px'}} onClick={() => setSongsCount(songsCount + 1)}>Increase Songs {songsCount} By 1</button>
         </div>
     );
 }
