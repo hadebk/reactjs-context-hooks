203a134ff940118f81c8b0da11c08344728e9d79
abdalhadi bakr <hade.bk.1998@gmail.com>
2020-05-27 23:58:14 +0200

    finish example (learning)

--- context-api/src/components/Booklist.js
+++ context-api/src/components/Booklist.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
-class Booklist extends Component {
-    static contextType = ThemeContext;
-    state = {  }
-    render() {
-        /**
-         * way 1 to access context
-         */
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return ( 
-            <div className="book-list" style={{background: theme.bg, color: theme.syntax}}>
-                <ul>
-                    <li style={{background: theme.ui}}>The way of kings</li>
-                    <li style={{background: theme.ui}}>The name of the wind</li>
-                    <li style={{background: theme.ui}}>The final empire</li>
-                </ul>
-            </div>
-         );
-    }
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
+  const theme = isLightTheme ? light : dark;
+  return ( 
+    <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+      <ul>
+        {books.map(book => {
+          return (
+            <li key={book.id} style={{ background: theme.ui }}>{book.title}</li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
  
-export default Booklist;
+export default BookList;
+
+
+/**
+ * way 1 to access context.
+ * this way of invoke context, can be used with class component only
+*/
+
+/*
+class BookList extends Component {
+   static contextType = ThemeContext;
+   state = {  }
+   render() {
+       const { isLightTheme, light, dark } = this.context;
+       const theme = isLightTheme ? light : dark;
+       return ( 
+           <div className="book-list" style={{background: theme.bg, color: theme.syntax}}>
+               <ul>
+                   <li style={{background: theme.ui}}>The way of kings</li>
+                   <li style={{background: theme.ui}}>The name of the wind</li>
+                   <li style={{background: theme.ui}}>The final empire</li>
+               </ul>
+           </div>
+        );
+   }
+}
+
+*/
--- context-api/src/components/Navbar.js
+++ context-api/src/components/Navbar.js
@@ -1,14 +1,44 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext.js';
+import { AuthContext } from '../contexts/AuthContext.js';
 
+const Navbar = () => {
+  /**
+   * use 2 context in one fun component 
+   * this way of invoke context, can be used with fun component only!
+   * @Note this way is useful when you want to use a lot of context in one component
+  */
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <div onClick={() => toggleAuth()}>
+        { isAuthenticated ? 'Logged in' : 'Logged out' }
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+   
+export default Navbar;
+
+/**
+ * way 2 to access context
+ * this way can be used in both class/fun component
+ */
+
+ /*
 class Navbar extends Component {
     static contextType = ThemeContext;
     state = {  }
     render() {
         return (
-            /**
-             * way 2 to access context
-             */
             <ThemeContext.Consumer>
                 {(context) => {
                     const { isLightTheme, light, dark } = context;
@@ -28,5 +58,4 @@ class Navbar extends Component {
         );
     }
 }
- 
-export default Navbar;
+*/
